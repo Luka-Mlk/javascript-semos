1ce4e2643aa1d9ced1474bbbf9b6bfef94f61993
Luka-Mlk <Luka-Mlk@users.noreply.github.com>
2022-11-09 19:34:29 +0100

    dodadeno mnozenje

--- domasni/kalkulator/index.js
+++ domasni/kalkulator/index.js
@@ -9,6 +9,9 @@ if ((a == 0 || b == 0) && znak == "/") {
 } else if (znak === "-") {
   let razlika = Number(a) - Number(b);
   console.log(razlika);
+} else if (znak === "*") {
+  let zbir = Number(a) * Number(b);
+  console.log(zbir);
 } else if (znak === "/") {
   let kolicnik = Number(a) / Number(b);
   console.log(kolicnik);
