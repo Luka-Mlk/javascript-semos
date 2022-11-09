e297641e6fc4aba72586899bb015d3fc552d4ec9
Luka-Mlk <Luka-Mlk@users.noreply.github.com>
2022-11-09 17:46:53 +0100

    kakulator domasna

---
+++ domasni/kalkulator/index.js
@@ -0,0 +1,18 @@
+const a = prompt();
+const znak = prompt();
+const b = prompt();
+if ((a == 0 || b == 0) && znak == "/") {
+  console.log("nedozvolen znak");
+} else if (znak === "+") {
+  let suma = Number(a) + Number(b);
+  console.log(suma);
+} else if (znak === "-") {
+  let razlika = Number(a) - Number(b);
+  console.log(razlika);
+} else if (znak === "/") {
+  let kolicnik = Number(a) / Number(b);
+  console.log(kolicnik);
+} else if (znak === "%") {
+  let ostatok = Number(a) % Number(b);
+  console.log(ostatok);
+}
